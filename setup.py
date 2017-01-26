0dd1c6a07f64de065a69ead07466765dd4555921
Emil Persson <emil.persson@creuna.se>
2017-01-26 16:17:09 +0100

    Bump to 0.1.0

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='py_ioc',
-    version='0.0.0',
+    version='0.1.0',
     description='Inversion of Control Container '
                 'for Automatic Dependency Injection',
     author='Emil Person',
     author_email='emil.n.persson@gmail.com',
     url='https://github.com/emilniklas/py_ioc',
-    download_url='https://github.com/emilniklas/py_ioc/tarball/0.0.0',
+    download_url='https://github.com/emilniklas/py_ioc/tarball/0.1.0',
     keywords=['ioc', 'di', 'dependency injection', 'inversion of control'],
     packages=find_packages(exclude=('tests')),
     classifiers=[
